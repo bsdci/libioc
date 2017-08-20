19fe64186990c9c58fea989f718178762524ff11
Igor Galić <i.galic@brainsware.org>
2017-08-20 17:05:15 +0200

    chore(pep8): cleanup libiocage/tests

--- libiocage/tests/helper_functions.py
+++ libiocage/tests/helper_functions.py
@@ -3,6 +3,7 @@ def _delete_dataset_recursive(dataset):
         _delete_dataset_recursive(child)
     dataset.delete()
 
+
 def unmount_and_destroy_dataset_recursive(dataset):
     dataset.umount_recursive()
     _delete_dataset_recursive(dataset)
--- libiocage/tests/test_Datasets.py
+++ libiocage/tests/test_Datasets.py
@@ -1,24 +1,21 @@
 import pytest
-import uuid
 
 import libiocage.lib
 
-import helper_functions
 
 class TestDatasets(object):
 
     @pytest.fixture
     def MockedDatasets(self, logger, pool):
-        
+
         class DatasetsMock(libiocage.lib.Datasets.Datasets):
             ZFS_POOL_ACTIVE_PROPERTY = "org.freebsd.ioc-test:active"
-        
+
         yield DatasetsMock
 
         prop = DatasetsMock.ZFS_POOL_ACTIVE_PROPERTY
         pool.root_dataset.properties[prop].value = "no"
 
     def test_pool_can_be_activated(self, MockedDatasets, pool, logger):
-        
         datasets = MockedDatasets(pool=pool, logger=logger)
         datasets.activate()
