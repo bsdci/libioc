70ad84ac10fc4bcfd1f6a4dd9343f5f5117fd33a
Stefan Grönke <stefan@gronke.net>
2017-08-25 18:08:54 +0200

    do not raise exception when cli/start fails

--- libiocage/cli/start.py
+++ libiocage/cli/start.py
@@ -51,7 +51,6 @@ def cli(ctx, rc, jails, log_level):
         try:
             jail.start()
         except Exception:
-            raise
             exit(1)
 
         logger.log(f"{jail.humanreadable_name} running as JID {jail.jid}")
