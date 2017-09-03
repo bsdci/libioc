a16b746774f7d0e24ee2fdb9d23cf94237383037
Igor Galić <igalic@brainsware.at>
2017-09-03 22:41:40 +0000

    preserve indention level

--- libiocage/lib/Logger.py
+++ libiocage/lib/Logger.py
@@ -193,7 +193,10 @@ class Logger:
             "\r",
             f"\033[{delta}F",  # CPL - Cursor Previous Line
             "\r",               # CR - Carriage Return
-            self._indent(f"{log_entry.message}: {delta}"),
+            self._indent(
+                f"{log_entry.message}: {delta}",
+                log_entry.indent
+            ),
             "\033[K",           # EL - Erase in Line
             "\n" * (delta),
             "\r"
