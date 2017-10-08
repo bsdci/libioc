04e36967c86a1e54d9f1a1a87485ee48fdd17166
Stefan Grönke <stefan@gronke.net>
2017-10-08 03:06:16 +0200

    Feature: cli/restart

---
+++ iocage/cli/restart.py
@@ -0,0 +1,90 @@
+# Copyright (c) 2014-2017, iocage
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted providing that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+"""start module for the cli."""
+import click
+
+import iocage.lib.errors
+import iocage.lib.Jails
+import iocage.lib.Logger
+
+__rootcmd__ = True
+
+
+@click.command(name="restart", help="Restarts the specified jails or ALL.")
+@click.pass_context
+@click.argument("jails", nargs=-1)
+def cli(ctx, jails):
+    """
+    Starts Jails
+    """
+
+    logger = ctx.parent.logger
+    print_function = ctx.parent.print_events
+
+    if len(jails) == 0:
+        logger.error("No jail selector provided")
+        exit(1)
+
+    jails = iocage.lib.Jails.JailsGenerator(
+        logger=logger,
+        filters=jails
+    )
+
+    restart_jails(jails, logger=logger, print_function=print_function)
+
+
+def restart_jails(jails, logger, print_function):
+
+    changed_jails = []
+    failed_jails = []
+    for jail in jails:
+
+        failed = False
+
+        try:
+            print_function(jail.stop())
+        except:
+            failed = True
+            pass
+
+        try:
+            print_function(jail.start())
+        except:
+            failed = True
+            pass
+
+        if failed is True:
+            failed_jails.append(jail)
+            continue
+
+        logger.log(f"{jail.humanreadable_name} restarted as JID {jail.jid}")
+        changed_jails.append(jail)
+
+    if len(failed_jails) > 0:
+        exit(1)
+
+    if len(changed_jails) == 0:
+        jails_input = " ".join(list(jails))
+        logger.error(f"No jails matched your input: {jails_input}")
+        exit(1)
