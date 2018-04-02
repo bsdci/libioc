5cee27f5721574a91cd80f44c8df45c07e4abaa3
Stefan Grönke <stefan@gronke.net>
2018-04-02 13:38:51 +0000

    remove unnecessary ZFS share umount

--- iocage/lib/Jail.py
+++ iocage/lib/Jail.py
@@ -575,7 +575,6 @@ class JailGenerator(JailResource):
         jailNetworkTeardownEvent = events.JailNetworkTeardown(self)
         jailServicesStopEvent = events.JailServicesStop(self)
         jailMountTeardownEvent = events.JailMountTeardown(self)
-        JailZfsShareUmount = events.JailZfsShareUmount(jail=self)
 
         self._run_hook("prestop")
 
@@ -596,15 +595,6 @@ class JailGenerator(JailResource):
         yield jailMountTeardownEvent.begin()
         self._teardown_mounts()
         yield jailMountTeardownEvent.end()
-
-        if self.config["jail_zfs"] is True:
-            yield JailZfsShareUmount.begin()
-            share_storage = iocage.lib.ZFSShareStorage.ZFSShareStorage(
-                jail=self,
-                logger=self.logger
-            )
-            share_storage.umount_zfs_shares()
-            yield JailZfsShareUmount.end()
 
         self.state.query()
 
@@ -790,7 +780,6 @@ class JailGenerator(JailResource):
         jailDestroyEvent = events.JailDestroy(self)
         jailNetworkTeardownEvent = events.JailNetworkTeardown(self)
         jailMountTeardownEvent = events.JailMountTeardown(self)
-        JailZfsShareUmount = events.JailZfsShareUmount(jail=self)
 
         try:
             self._run_hook("prestop")
@@ -821,18 +810,6 @@ class JailGenerator(JailResource):
             yield jailMountTeardownEvent.end()
         except Exception as e:
             yield jailMountTeardownEvent.skip()
-
-        if self.config["jail_zfs"] is True:
-            yield JailZfsShareUmount.begin()
-            try:
-                share_storage = iocage.lib.ZFSShareStorage.ZFSShareStorage(
-                    jail=self,
-                    logger=self.logger
-                )
-                share_storage.umount_zfs_shares()
-                yield JailZfsShareUmount.end()
-            except Exception as e:
-                yield JailZfsShareUmount.skip()
 
         try:
             self.state.query()
