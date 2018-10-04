a95c17c3fbc24f8720b8184207407d26b13318ff
Stefan Grönke <stefan@gronke.net>
2018-10-04 18:45:17 +0200

    bump version v0.3.1

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ if sys.version_info < (3, 6):
 setup(
     name='iocage',
     license='BSD',
-    version='0.3.0',
+    version='0.3.1',
     description='A Python library to manage jails with iocage',
     keywords='FreeBSD jail iocage',
     author='iocage Contributors',
@@ -101,7 +101,7 @@ setup(
 setup(
     name='ioc',
     license='BSD',
-    version='0.3.0',
+    version='0.3.1',
     description='A Python library to manage jails with iocage',
     keywords='FreeBSD jail iocage',
     author='iocage Contributors',
