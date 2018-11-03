ab6bd9975eef7db209dba13f69cfd22cb165dd82
Stefan Grönke <stefan@gronke.net>
2018-11-03 21:49:01 +0000

    address flake8 complaints in helpers

--- iocage/helpers.py
+++ iocage/helpers.py
@@ -42,7 +42,7 @@ CommandOutput = typing.Tuple[typing.Optional[str], typing.Optional[str], int]
 
 
 def get_os_version(
-        version_file: str="/bin/freebsd-version"
+    version_file: str="/bin/freebsd-version"
 ) -> typing.Dict[str, typing.Union[str, int, float]]:
     """Get the hosts userland version."""
     f = open(version_file, "r", re.MULTILINE, encoding="utf-8")
