e27cd57cc8e63ee2c4e73668e59a94700c7b9a5a
Stefan Grönke <stefan@gronke.net>
2019-01-05 03:47:26 +0000

    improve ioc module docstring
    addresses https://github.com/bsdci/libioc/pull/598#discussion_r245478227

--- ioc/__init__.py
+++ ioc/__init__.py
@@ -22,7 +22,7 @@
 # STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
 # IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-"""Libioc Python module ioc."""
+"""Python jail management module using ioc{age,ell}."""
 import sys
 import os.path
 import importlib
