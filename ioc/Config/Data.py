050d7ed8b5dae1a25adcdbf12a19035394ee331a
Stefan Grönke <stefan@gronke.net>
2019-01-08 14:40:24 +0100

    correct Config.Data module docstring

--- ioc/Config/Data.py
+++ ioc/Config/Data.py
@@ -22,7 +22,7 @@
 # STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
 # IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-"""The common base of jail configurations."""
+"""Internal data storage for BaseConfig objects."""
 import typing
 import collections.abc
 
