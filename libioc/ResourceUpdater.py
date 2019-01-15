52a49d0bbb453b5f15da2065adae8e483c693214
Igor Galić <igalic@brainsware.at>
2019-01-15 15:51:17 +0100

    Fix pkg install and freebsd update
    during the review of #602, we decided to rename the Jail class variable
    `ignore_user_defaults` to `ignore_source_config`, so as to not be confused
    with `user.*` properties.
    
    This was, however, not done consequentially throughout the PR, so that
    `ioc pkg` and `ioc update` were still broken.
    
    This PR fixes that.

--- libioc/ResourceUpdater.py
+++ libioc/ResourceUpdater.py
@@ -141,7 +141,7 @@ class Updater:
                 dataset=self.resource.dataset
             )
             temporary_jail.config.file = "config_update.json"
-            temporary_jail.config.ignore_user_defaults = True
+            temporary_jail.config.ignore_source_config = True
 
             root_path = temporary_jail.root_path
             destination_dir = f"{root_path}{self.local_release_updates_dir}"
