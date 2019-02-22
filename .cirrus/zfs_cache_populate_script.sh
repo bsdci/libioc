1f6435f0ca877244f2e52e82646def0e0bd15dbe
Stefan Grönke <stefan@gronke.net>
2019-02-22 13:35:49 +0100

    re-apply Cirrus Ci zfs_cache.populate script to .cirrus.yml

--- .cirrus/zfs_cache_populate_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-print "Cirrus Ci: populate ZFS pool $POOL"
-mkdir -p /pools/
-truncate -s 16G "/pools/$POOL.img"
-zpool create -m "/.$POOL" "$POOL" "/pools/$POOL.img"
-zfs set compression=lz4 "$POOL"
-zpool export "$POOL"
