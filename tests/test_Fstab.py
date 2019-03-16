44ab0b04c672d6af54963cb94c8fe31d785326fb
Stefan Grönke <stefan@gronke.net>
2019-03-16 16:22:56 +0000

    unit test Config.Jail.File.Fstab

---
+++ tests/test_Fstab.py
@@ -0,0 +1,91 @@
+# Copyright (c) 2017-2019, Stefan Grönke
+# Copyright (c) 2014-2018, iocage
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted providing that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+"""Unit tests for Fstab module."""
+import tempfile
+
+import libioc.Config.Jail.File.Fstab
+
+class TestFstab(object):
+	"""Run Fstab unit tests."""
+
+	def test_can_read_files_with_utf8(
+		self
+	) -> None:
+
+		temp_file = tempfile.NamedTemporaryFile()
+		content = "# Stefan Grönke was here."
+		temp_file.write(content.encode("utf-8"))
+		temp_file.seek(0)
+
+		fstab = libioc.Config.Jail.File.Fstab.Fstab(file=temp_file.name)
+
+		assert len(fstab) == 1
+
+	def test_can_read_files_with_comment_and_fstab_line(
+		self
+	) -> None:
+
+		temp_file = tempfile.NamedTemporaryFile()
+		content = """# This is a comment\twith tabs
+fdescfs /dev/fd fdescfs rw	0 0"""
+		assert content.count("\n") == 1
+		
+		temp_file.write(content.encode("utf-8"))
+		temp_file.seek(0)
+
+		fstab = libioc.Config.Jail.File.Fstab.Fstab(file=temp_file.name)
+
+		assert len(fstab) == 2
+		assert type(fstab[0]).__name__ == "FstabCommentLine"
+		assert str(fstab[0]) == "# This is a comment\twith tabs"
+		assert type(fstab[1]).__name__ == "FstabLine"
+		assert str(fstab[1]) == "fdescfs\t/dev/fd\tfdescfs\trw\t0\t0"
+
+	def test_parses_auto_lines(
+		self
+	) -> None:
+
+		AUTO_COMMENT_IDENTIFIER = "iocage-auto"
+		temp_file = tempfile.NamedTemporaryFile()
+		content = f"""# This is a comment\twith tabs
+fdescfs /dev/fd fdescfs rw	0 0 # {AUTO_COMMENT_IDENTIFIER}
+tmpfs /tmp tmpfs rw,mode=777 0 0 # {AUTO_COMMENT_IDENTIFIER}
+# yet another comment line
+tmpfs /tmp tmpfs rw,mode=777 0 0 # {AUTO_COMMENT_IDENTIFIER}"""
+		assert content.count("\n") == 4
+		
+		temp_file.write(content.encode("utf-8"))
+		temp_file.seek(0)
+
+		fstab = libioc.Config.Jail.File.Fstab.Fstab(file=temp_file.name)
+
+		# only 3 lines are expected
+		assert len(fstab) == 3
+
+		assert type(fstab[0]).__name__ == "FstabCommentLine"
+		assert str(fstab[0]) == "# This is a comment\twith tabs"
+		assert type(fstab[1]).__name__ == "FstabAutoPlaceholderLine"
+		assert type(fstab[2]).__name__ == "FstabCommentLine"
+		assert str(fstab[2]) == "# yet another comment line"
