249476224893372e611ed9bbd0b1225ba7caa824
Stefan Grönke <stefan@gronke.net>
2019-04-21 11:16:03 +0200

    auto-generate docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.6.0 2019/02/08',
+    VERSION: '0.8.0 2019/04/13',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
