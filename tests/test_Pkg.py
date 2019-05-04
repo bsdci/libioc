32bc924c9df6a3e0ea2178fd01e91c8ee38fc177
Stefan Grönke <stefan@gronke.net>
2019-05-04 10:23:39 +0000

    consistently use spaces in Pkg tests

--- tests/test_Pkg.py
+++ tests/test_Pkg.py
@@ -35,64 +35,64 @@ import libioc.Pkg
 
 class TestPkg(object):
 
-	@pytest.fixture(scope="function")
-	def pkg(
-		self,
-		host: 'libioc.Host.HostGenerator',
-		logger: 'libioc.Logger.LoggerGenerator',
-		zfs: 'libioc.ZFS.ZFS'
-	) -> libioc.Pkg.Pkg:
-		return libioc.Pkg.Pkg(
-			host=host,
-			logger=logger,
-			zfs=zfs
-		)
+    @pytest.fixture(scope="function")
+    def pkg(
+        self,
+        host: 'libioc.Host.HostGenerator',
+        logger: 'libioc.Logger.LoggerGenerator',
+        zfs: 'libioc.ZFS.ZFS'
+    ) -> libioc.Pkg.Pkg:
+        return libioc.Pkg.Pkg(
+            host=host,
+            logger=logger,
+            zfs=zfs
+        )
 
-	def test_can_mirror_packages(
-		self,
-		pkg: libioc.Pkg.Pkg,
-		release: 'libioc.Release.ReleaseGenerator',
-		root_dataset: libzfs.ZFSDataset
-	) -> None:
+    def test_can_mirror_packages(
+        self,
+        pkg: libioc.Pkg.Pkg,
+        release: 'libioc.Release.ReleaseGenerator',
+        root_dataset: libzfs.ZFSDataset
+    ) -> None:
 
-		major_version = int(release.version_number)
-		local_pkg_dir = f"{root_dataset.mountpoint}/pkg/{major_version}"
+        major_version = int(release.version_number)
+        local_pkg_dir = f"{root_dataset.mountpoint}/pkg/{major_version}"
 
-		list(pkg.fetch(
-			["sl", "git-lite"],
-			release=release
-		))
+        list(pkg.fetch(
+            ["sl", "git-lite"],
+            release=release
+        ))
 
-		assert os.path.exists(local_pkg_dir)
-		assert os.path.isdir(local_pkg_dir)
-		assert os.path.exists(
-			f"{local_pkg_dir}/repos/ioc-release-{major_version}.conf"
-		)
+        assert os.path.exists(local_pkg_dir)
+        assert os.path.isdir(local_pkg_dir)
+        assert os.path.exists(
+            f"{local_pkg_dir}/repos/ioc-release-{major_version}.conf"
+        )
 
-		cache_dir_index = os.listdir(f"{local_pkg_dir}/cache")
-		assert any([x.startswith("sl") for x in cache_dir_index])
-		assert any([x.startswith("git-lite") for x in cache_dir_index])
+        cache_dir_index = os.listdir(f"{local_pkg_dir}/cache")
+        assert any([x.startswith("sl") for x in cache_dir_index])
+        assert any([x.startswith("git-lite") for x in cache_dir_index])
 
-	def test_can_install_packages_to_stopped_jail(
-		self,
-		pkg: libioc.Pkg.Pkg,
-		release: 'libioc.Release.ReleaseGenerator',
-		existing_jail: 'libioc.Jail.Jail'
-	) -> None:
-		packages = ["sl"]
-		list(pkg.fetch(packages, release=release))
+    def test_can_install_packages_to_stopped_jail(
+        self,
+        pkg: libioc.Pkg.Pkg,
+        release: 'libioc.Release.ReleaseGenerator',
+        existing_jail: 'libioc.Jail.Jail'
+    ) -> None:
+        packages = ["sl"]
+        list(pkg.fetch(packages, release=release))
 
-		jail_command_event = None
-		for event in pkg.install(packages=packages, jail=existing_jail):
-			if isinstance(event, libioc.events.JailCommand) and event.done:
-				jail_command_event = event
-		assert jail_command_event is not None
-		assert os.path.exists(f"{existing_jail.root_path}/usr/local/bin/sl")
+        jail_command_event = None
+        for event in pkg.install(packages=packages, jail=existing_jail):
+            if isinstance(event, libioc.events.JailCommand) and event.done:
+                jail_command_event = event
+        assert jail_command_event is not None
+        assert os.path.exists(f"{existing_jail.root_path}/usr/local/bin/sl")
 
-		assert existing_jail.running is False
+        assert existing_jail.running is False
 
-		pkg_mountpoint = existing_jail.root_path + "/.ioc-pkg"
-		stdout = subprocess.check_output(
+        pkg_mountpoint = existing_jail.root_path + "/.ioc-pkg"
+        stdout = subprocess.check_output(
             [f"/sbin/mount"]
         ).decode("utf-8")
         assert str(pkg_mountpoint) not in stdout
